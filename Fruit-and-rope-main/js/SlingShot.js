6adb76e5714a0ee8f0cdff1f0ad7795df9a03472
Jijori <102591973+Jijori@users.noreply.github.com>
2023-01-28 12:38:48 +0530

    Add files via upload

---
+++ Fruit-and-rope-main/js/SlingShot.js
@@ -0,0 +1,31 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 100
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+
+        World.add(world, this.sling);
+
+    }
+    fly(){
+        //null means nothing
+        this.sling.bodyA = null;
+    }
+    display(){
+        //if the slingshot is not null, it display the code below
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("yellow");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        }
+    
+}
